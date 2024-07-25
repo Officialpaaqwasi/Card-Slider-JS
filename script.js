40e57f68d448a85ddba415c4352f305fd68a7b34
officialpaaqwasi <iklutse@st.ug.edu.gh>
2024-07-25 13:45:44 +0000

    25/07/24 phase1

---
+++ script.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.slider-wrapper', { 
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //Responsive breakpoints
+  breakpoints:{
+   0: {
+    slidesPerView: 1
+   },
+   768: {
+    slidesPerView: 2
+   },
+   1024: {
+    slidesPerView: 3
+   }
+  }
+});
